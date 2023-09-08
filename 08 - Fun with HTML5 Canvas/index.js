b2b7ec2fab940f7e79cf3af649e6c0b5cc09524c
Prakhar Yadav <yadavprakhar1809@gmail.com>
2023-09-08 21:35:30 +0530

    Day 8 of JavaScript 30-Day-Challenge

---
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "black";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth=20;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue=0;
+
+function draw(e) {
+  if (!isDrawing) return;
+//   ctx.strokeStyle= `hsl(${hue},100%,50%)`
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+//   hue++;
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+});
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+canvas.addEventListener("mouseout", () => (isDrawing = false));
